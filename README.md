d406a0cfc5d152ab7056103f07e658f198315c47
Flaque <econrad@zagmail.gonzaga.edu>
2018-04-25 14:35:10 -0700

    Add readme and initial code

---
+++ README.md
@@ -0,0 +1,16 @@
+# pre 
+
+Pre is a simple command line tool for adding text to the beginning of a file. 
+
+You previously could do this ["easily"](https://media.giphy.com/media/ANbD1CCdA3iI8/giphy.gif) with `sed`:
+
+```bash
+$ sed -i.old '1s;^;to be prepended;' inFile
+```
+
+But now you can just do it with `pre`:
+
+```bash 
+$ echo "some text in front" | pre someFile.txt 
+```
+
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func append(content string, filename string) string {
+
+	// Read
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file '%s'\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Output
+	return content + string(b)
+}
+
+const helpText = "'pre' listens to stdin and appends it to a file.\n\n" +
+	"Usage: pre FILENAME \n" +
+	"Example: 'cat puppers.txt | pre cute-things.txt'"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println(helpText)
+		os.Exit(1)
+	}
+
+	// Check if file exists
+	if _, err := os.Stat(os.Args[1]); os.IsNotExist(err) {
+		fmt.Printf("File '%s' does not exist.\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	total := ""
+	for scanner.Scan() {
+		total += scanner.Text()
+	}
+	total += "\n"
+
+	output := append(total, os.Args[1])
+	fmt.Println(output)
+}
